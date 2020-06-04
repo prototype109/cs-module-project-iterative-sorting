af24381bf8d23e2a08f2bdd79977c9918a71d3eb
Brandon Nichols <brandonaronnichols@gamil.com>
2020-06-03 23:04:23 -0500

    adding searching.py to the commit

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +10,18 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
+    low = 0
+    middle_index = 0
+    high = len(arr) - 1
     # Your code here
+    while low <= high:
+        middle_index = (high + low)//2
 
+        if arr[middle_index] < target:
+            low = middle_index + 1
+        elif arr[middle_index] > target:
+            high = middle_index - 1
+        else:
+            return middle_index
 
     return -1  # not found
